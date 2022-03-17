d733e18c0f20aaf4f746adf8477cfc556258135c
Vincent <vshan0816@gmail.com>
2022-03-17 15:10:33 -0400

    fix order of seed creation, continue adding to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-Anime.create(title: "Demon Slayer" synopsis: "A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko, who is turning into a demon slowly. Tanjiro sets out to become a demon slayer to avenge his family and cure his sister." release_year: 2019 total_seasons: 2 image_url: "https://static.wikia.nocookie.net/toonami/images/3/33/Demon_Slayer.jpg/revision/latest?cb=20190705214700" studio_id: 1 )
-Anime.create(title: "Fate/Zero" synopsis: "Seven chosen mages and their summoned heroic spirits fight against each other to try and win the Holy Grail: a magical device that can grant any wish." release_year: 2011 total_seasons: 1 image_url: "https://img.betaseries.com/_nNp3rJK-5wamT7h695pLd5VRr8=/400x600/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fab1626e6894123fc69ade5f63fd25bd7.jpg" studio_id: 1)
+Studio.create(name: "Ufotable" year_founded: 2000 )
+Studio.create(name: "Madhouse" year_founded: 1972)
+Studio.create(name: "" year_founded: )
+Studio.create(name: "" year_founded: )
+
+Anime.create(title: "Demon Slayer" synopsis: "A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko, who is turning into a demon slowly. Tanjiro sets out to become a demon slayer to avenge his family and cure his sister." release_year: 2019 total_seasons: 2 image_url: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg" studio_id: 1 )
+Anime.create(title: "Fate/Zero" synopsis: "Seven chosen mages and their summoned heroic spirits fight against each other to try and win the Holy Grail: a magical device that can grant any wish." release_year: 2011 total_seasons: 2 image_url: "https://cdn.myanimelist.net/images/anime/3/35097.jpg" studio_id: 1)
+Anime.create(title: "God Eater" synopsis: "The year 2071, the Earth has been devastated by mysterious monsters that devour living creatures and are impervious to weapons. An organization called Fenrir is the only one with weapons capable of killing the monsters." release_year: 2015 total_seasons: 1 image_url: "https://cdn.myanimelist.net/images/anime/7/73852.jpg" studio_id: 1 )
+Anime.create(title: "One Punch Man" synopsis: "Saitama, the protagonist, is an exceptionally powerful hero who easily defeats the monsters or other villains with a single punch. However, due to his overwhelming strength, Saitama has become bored with his powers and is constantly trying to find stronger opponents who can fight him." release_year: 2015 total_seasons: 2 image_url: "https://cdn.myanimelist.net/images/anime/7/72533.jpg" studio_id: 2 )
+Anime.create(title: "Black Lagoon" synopsis: "The story follows a team of pirates/mercenaries known as Lagoon Company, that smuggles goods in and around the seas of Southeast Asia in the early 1990s." release_year: 2006 total_seasons: 3 image_url: "https://cdn.myanimelist.net/images/anime/10/20980.jpg" studio_id: 2 )
+Anime.create(title: "Death Note" synopsis: "Based on the famous Japanese manga written by Tsugumi Ohba and Takeshi Obata, Death Note follows a high school student who comes across a supernatural notebook, realizing it holds within it a great power; if the owner inscribes someone's name into it while picturing their face, he or she will die." release_year: 2006 total_seasons: 1 image_url: "https://cdn.myanimelist.net/images/anime/9/9453.jpg" studio_id: 2 )
+Anime.create(title: "One Outs" synopsis: "A successful gambler and pitcher in the game called 'One Outs'. After losing a game of One Outs for the first time ever to Kojima, he joins the Lycaons with an odd contract, the One Outs contract, that decides his pay based on his performance." release_year: 2008 total_seasons: 1 image_url: "https://cdn.myanimelist.net/images/anime/13/21048.jpg" studio_id: 2 )
 Anime.create(title: "" synopsis: "" release_year: total_seasons: image_url: studio_id: )
 Anime.create(title: "" synopsis: "" release_year: total_seasons: image_url: studio_id: )
 Anime.create(title: "" synopsis: "" release_year: total_seasons: image_url: studio_id: )
 
-Studio.create(name: "Ufotable" year_founded: 2000 )
+
