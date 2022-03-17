7015b23746a508a8ebddae9d6531901599cd703f
Vincent <vshan0816@gmail.com>
2022-03-17 16:32:12 -0400

    add basic presence validations

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,3 +1,6 @@
 class Anime < ActiveRecord::Base
     belongs_to: studio
+    validates :title, presence: true
+    validates :synopsis, presence: true
+    validates :release_year, presence: true
 end
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,5 @@
 class Studio < ActiveRecord::Base
     has_many: animes
+    validates :name, presence: true
+    
 end
