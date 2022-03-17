44656d4d8150b8821b79adeff107e8affbaa03f8
Vincent <vshan0816@gmail.com>
2022-03-17 16:43:16 -0400

    comment out associations and validations, change migrations to inherit from activerecord v 5.2.

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,5 +1,5 @@
 class Studio < ActiveRecord::Base
-    has_many: animes
-    validates :name, presence: true
+    # has_many : animes
+    # validates :name, presence: true
     
 end
--- db/migrate/20220317170429_create_animes.rb
+++ db/migrate/20220317170429_create_animes.rb
@@ -1,4 +1,4 @@
-class CreateAnimes < ActiveRecord::Migration
+class CreateAnimes < ActiveRecord::Migration[5.2]
   def change
     create_table :animes do |t|
       t.string :title
