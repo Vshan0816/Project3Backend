769b12ccc1ed52c59b9795338d070117636c5ea9
Vincent <vshan0816@gmail.com>
2022-03-23 13:47:28 -0400

    create CRUD for animes_controller, add private methods

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,28 +2,62 @@ class AnimesController < ApplicationController
 
   # GET: /animes
   get "/animes" do
-   
+    Anime.all.to_json(include: [studio: {except: [:created_at, :updated_at]}], except: [:created_at, :updated_at])
   end
 
 
   # POST: /animes
   post "/animes" do
-   
+    @anime = Anime.create(params)
+    if @anime.id
+      serialized_anime
+    else
+      @anime.errors.full_messages.to_sentence
+    end
   end
 
   # GET: /animes/5
   get "/animes/:id" do
+    find_anime
+    if @anime
+      serialized_anime
+    else
+      {errors: "Record not found with id #{params['id']}"}.to_json
+    end
   
   end
 
 
   # PATCH: /animes/5
   patch "/animes/:id" do
-  
+    find_anime
+    if @anime && @anime.update(params)
+      serialized_anime
+    elsif !@anime
+      {errors: "Record not found with id #{params['id']}"}.to_json
+    else
+      {errors: @anime.errors.full_messages.to_sentence}.to_json
+    end
   end
 
   # DELETE: /animes/5/delete
   delete "/animes/:id/delete" do
-  
+    find_anime
+    if @anime&.destroy
+      {messages: "Record successfully destroyed"}.to_json
+    else
+      {errors: "Record not found with id #{params['id']}"}.to_json
+    end
+  end
+end
+
+  private
+
+  def find_anime
+    @anime = Anime.find_by_id(params["id"])
+  end
+
+  def serialized_anime
+    @anime.to_json(include: :studio)
   end
 end
--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,31 +7,31 @@ class StudiosController < ApplicationController
 
   # GET: /studios/new
   get "/studios/new" do
-    erb :"/studios/new.html"
+   
   end
 
   # POST: /studios
   post "/studios" do
-    redirect "/studios"
+  
   end
 
   # GET: /studios/5
   get "/studios/:id" do
-    erb :"/studios/show.html"
+  
   end
 
   # GET: /studios/5/edit
   get "/studios/:id/edit" do
-    erb :"/studios/edit.html"
+  
   end
 
   # PATCH: /studios/5
   patch "/studios/:id" do
-    redirect "/studios/:id"
+  
   end
 
   # DELETE: /studios/5/delete
   delete "/studios/:id/delete" do
-    redirect "/studios"
+  
   end
 end
